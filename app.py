4e27753c830934e1951fb537ed872060e593d7e4
asklurk <asklurkai@gmail.com>
2025-10-05 21:42:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,18 +26,6 @@ google = oauth.register(
 )
 
 
-github = oauth.register (
-  name = 'github',
-    client_id = app.config["GITHUB_CLIENT_ID"],
-    client_secret = app.config["GITHUB_CLIENT_SECRET"],
-    access_token_url = 'https://github.com/login/oauth/access_token',
-    access_token_params = None,
-    authorize_url = 'https://github.com/login/oauth/authorize',
-    authorize_params = None,
-    api_base_url = 'https://api.github.com/',
-    client_kwargs = {'scope': 'user:email'},
-)
-
 
 # Default route
 @app.route('/')
@@ -63,22 +51,6 @@ def google_authorize():
     return "You are successfully signed in using google"
 
 
-# Github login route
-@app.route('/login/github')
-def github_login():
-    github = oauth.create_client('github')
-    redirect_uri = url_for('github_authorize', _external=True)
-    return github.authorize_redirect(redirect_uri)
-
-
-# Github authorize route
-@app.route('/login/github/authorize')
-def github_authorize():
-    github = oauth.create_client('github')
-    token = github.authorize_access_token()
-    resp = github.get('user').json()
-    print(f"\n{resp}\n")
-    return "You are successfully signed in using github"
 
 
 if __name__ == '__main__':
